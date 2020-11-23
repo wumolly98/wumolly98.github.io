a6b04ee7afabafaa4e0546dca40868787a4ebaf1
Molly Wu <62619013+wumolly98@users.noreply.github.com>
2020-11-22 19:28:34 -0800

    ugh unmute no work

--- homework6/js/video.js
+++ homework6/js/video.js
@@ -1,17 +1,21 @@
 var video = document.getElementById("myVideo");
 
 window.addEventListener("load", function() {
-	video = document.querySelector("#myVideo");
+    video = document.querySelector("#myVideo");
+    console.log("Starting");
+    console.log(video.volume);
 });
 
 document.querySelector("#play").addEventListener("click", function() {
-    video.play(); 
     console.log("Play Video");
+    video.play(); 
+    document.querySelector("#volume").innerHTML = 100*(video.volume) + "%";
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
+    console.log("Pause Video");
     video.pause();
-	console.log("Pause Video");
+
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
@@ -21,8 +25,8 @@ document.querySelector("#slower").addEventListener("click", function() {
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-    video.playbackRate = video.playbackRate;
-    video.playbackRate = (curSpeed) /.9;
+    var curSpeed = video.playbackRate;
+    video.playbackRate = (curSpeed)/.9;
     console.log("Current Speed is: " + curSpeed);
 });
 
@@ -35,5 +39,28 @@ document.querySelector("#skip").addEventListener("click", function() {
     }
     console.log("current time of the video: " + video.currentTime);
     video.play();
+    
+});
+
+document.querySelector("#volumeSlider").addEventListener("change", function(){
+    console.log(this);
+    console.log(this.value);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+    if (video.muted === false) {    
+        video.muted = true;
+ }
+    console.log("Mute Video");
+    video.mute(); 
+    
+});
+
+document.querySelector("#unmute").addEventListener("click", function() {
+    if (video.unmuted === false) {    
+        video.unmuted = true;
+ }
+    console.log("Unmute Video");
+    video.unmute(); 
     
 });
